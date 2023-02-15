a99d57a6e69e612b45480f456ccbe0cd26f254ff
Anish420 <>
2023-02-15 07:29:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+
+# THIS IS A TIP CALCULATOR APP 🤑🤑
+
+food_ammount = float(input("Enter your Food Ammount: $"))
+food_percentage = float(input("Enter your Food percentage: %")) /100
+tip_ammount = food_ammount * food_percentage
+
+# LETS MAKE THE TOTAL VALUE  💵 💵 💵 💵 💵 💵
+
+total = food_ammount + tip_ammount
+
+# LETS PRINT THOSE THINGS   [̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅]
+
+print("--------------------------------------------------")
+print(f"🍗Food Ammount: ${food_ammount}")
+print(f"📌Tip Ammount: ${tip_ammount}")
+print("\n")
+
+print(f"💸Total Ammount: ${total}")
+print("--------------------------------------------------")
+
+# THIS IS MY PROJECT THANKS EVERYONE AND KEEP UPSKILLING YOURSELF☺☺👍👍
